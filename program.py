5ab3d47f8af43435f598184314f1daf50864fa64
kornas <kenssske@yandex.ru>
2022-11-27 17:55:47 +0300

    Update copyright years

--- program.py
+++ program.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 # I am author!
-print("My first Git program!!!")
-# 2017 (c) Me
-=======
-print("Hello, python")
->>>>>>> python3branch
+    print("My first Git program!!!")
+    print("Hello, python")
+    # 2017-2018 (c) Me
